b8c1af289a0222aff1ab394849482a73a003bb1c
PlugAndCree <NicolaMarangon123@gmail.com>
2020-09-17 16:13:17 +0200

    Removed old compile.sh

--- compile.sh
+++
@@ -1,7 +0,0 @@
-nasm src.asm -f bin -o boot_sect.bin
-nasm kernel_entry.asm -f elf32 -o kernel_entry.o
-
-gcc -m32 -fno-pie -ffreestanding -c kernel.c -o kernel.o
-
-ld -m elf_i386 -o kernel.bin -Ttext 0x7e00 kernel_entry.o kernel.o --oformat binary
-cat boot_sect.bin kernel.bin > os-image.bin
